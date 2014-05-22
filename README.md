d477d80f33ab20912a5c27fbae0ee562661fb5b3
uneventan <adam@gea-interactive.com.au>
2014-05-22 19:22:20 +1000

    versioning to readme

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 gsag
 ====
+
+v0.0.3 (2014-05-22 17:05:48 +1000)
+  - dependency checks: TweenMax, $.fn.onScreen
+  - triggers: click, class
+  - foreign triggers: click
+  - functionality: final/intial durations, final/initial delay, nesting
+v0.0.2 (2014-05-22 17:04:51 +1000)
+  - defaults
+  - triggers: onScreen, hover
--- js/gsag.js
+++ js/gsag.js
@@ -27,18 +27,6 @@ https://github.com/meetselva/attrchange/blob/master/MIT-License.txt
 
 
 
-/*
-  v0.0.2 (2014-05-22 17:04:51 +1000)
-    - defaults
-    - triggers: onScreen, hover
-  v0.0.3 (2014-05-22 17:05:48 +1000)
-    - dependency checks: TweenMax, $.fn.onScreen
-    - triggers: click, class
-    - foreign triggers: click
-    - functionality: final/intial durations, final/initial delay, nesting
-*/
-
-
 (function($) {
   /* ----------------------------------------------------------------
     INIT
